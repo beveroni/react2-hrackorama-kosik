5dfe5f9eb7d75d005626c4c17af73bbd1e8b7ccf
Veronika Bendova <svechovaver@gmail.com>
2023-10-19 18:27:52 +0200

    State Lifting z Amount do komponenty CartItem

--- src/components/CartItem/index.jsx
+++ src/components/CartItem/index.jsx
@@ -1,14 +1,20 @@
-import Amount from "../Amount";
-import CartProduct from "../CartProduct";
+import Amount from '../Amount';
+import CartProduct from '../CartProduct';
+import { useState } from 'react';
 import './style.css';
 
 const CartItem = ({ product }) => {
+  const [count, setCount] = useState(product.amount);
+
+  const handleAmountChange = (newCount) => {
+    setCount(newCount);
+  };
   return (
     <div className="cart-item">
       <CartProduct name={product.name} price={product.price} />
-      <Amount value={product.amount} />
+      <Amount value={count} onChange={handleAmountChange} />
     </div>
-  )
+  );
 };
 
 export default CartItem;
